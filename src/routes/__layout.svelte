809659cd1746e8fa4fefd9c3bdb7c1006ff2b362
chandani-themesbrand <chandani@themesbrand.com>
2022-08-01 09:17:17 +0530

    sveltekit added

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,59 @@
+<script>
+	import { page } from '$app/stores';
+
+	const publicRoutes = ['/'];
+
+	$: isPublic = publicRoutes.includes($page.url.pathname);
+
+	import Header from '$lib/header/Header.svelte';
+	import '../app.css';
+
+</script>
+
+
+{#if isPublic}
+<Header />
+
+<main>
+	<slot />
+</main>
+
+<footer>
+	<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+</footer>
+{:else}
+  // authenticated layout
+  <slot />
+{/if}
+
+
+<style>
+	main {
+		flex: 1;
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		width: 100%;
+		max-width: 1024px;
+		margin: 0 auto;
+		box-sizing: border-box;
+	}
+
+	footer {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 40px;
+	}
+
+	footer a {
+		font-weight: bold;
+	}
+
+	@media (min-width: 480px) {
+		footer {
+			padding: 40px 0;
+		}
+	}
+</style>
